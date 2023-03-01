4919706a15fc5e06f1942307d7f83fd162ba67df
Karl Enteria <karl.enteria@gmail.com>
2023-03-02 01:34:21 +0800

    fixed tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,6 @@
-module.exports = (ctx) => {
-  // This flag is set when loading configuration by this package.
-  if (ctx.meteor) {
-    const config = {
-      plugins: {
-        "postcss-easy-import": {},
-      },
-    };
-
-    if (ctx.env === "production") {
-      // "autoprefixer" is reported to be slow,
-      // so we use it only in production.
-      config.plugins.autoprefixer = {
-        browsers: ["last 2 versions"],
-      };
-    }
-
-    return config;
-  } else {
-    return {};
-  }
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
