448b7b7d661da62c7211a10a15df682ff254a3b7
Karl Enteria <karl.enteria@gmail.com>
2023-03-01 21:02:29 +0800

    added tailwind

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ContactList from "./ContactList";
 import ContactsForm from "./ContactsForm";
+import Header from "./Header";
 
 //Contact: name, email, imageurl
 export const App = () => (
   <div>
-    <h1>Meteor Wallet - Galaxy</h1>
-    <ContactsForm />
-    <ContactList />
+    <Header />
+    <div className="min-h-full">
+      <div className="max-w-4xl mx-auto p-2">
+        <ContactsForm />
+        <ContactList />
+      </div>
+    </div>
   </div>
 );
--- imports/ui/ContactsForm.jsx
+++ imports/ui/ContactsForm.jsx
@@ -1,54 +1,79 @@
-import React, { useState } from "react";
+import React from "react";
 import { ContactsCollection } from "../api/ContactsCollection";
 
-const ContactsForm = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+const ContactForm = () => {
+  const [name, setName] = React.useState(""); // Formik
+  const [email, setEmail] = React.useState("");
+  const [imageUrl, setImageUrl] = React.useState("");
+
   const saveContact = () => {
-    console.log({ name, email, imageUrl });
     ContactsCollection.insert({ name, email, imageUrl });
     setName("");
     setEmail("");
     setImageUrl("");
   };
+
   return (
-    <>
-      <h2>Contacts Form</h2>
-      <form>
-        <div style={{ marginBottom: 3 }}>
-          <label htmlFor="name">Name</label>
+    <form className="mt-6">
+      <div className="grid grid-cols-6 gap-6">
+        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Name
+          </label>
           <input
             type="text"
-            value={name}
             id="name"
+            value={name}
             onChange={(e) => setName(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
         </div>
-        <div style={{ marginBottom: 3 }}>
-          <label htmlFor="email">Email</label>
+
+        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
           <input
             type="email"
-            value={email}
             id="email"
+            value={email}
             onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
         </div>
-        <div style={{ marginBottom: 3 }}>
-          <label htmlFor="imageUrl">Image</label>
+
+        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label
+            htmlFor="imageUrl"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Image URL
+          </label>
           <input
             type="text"
-            value={imageUrl}
             id="imageUrl"
+            value={imageUrl}
             onChange={(e) => setImageUrl(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
         </div>
-        <button type="button" onClick={saveContact}>
+      </div>
+      <div className="px-2 py-3 text-right">
+        <button
+          type="button"
+          onClick={saveContact}
+          className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+        >
           Save Contact
         </button>
-      </form>
-    </>
+      </div>
+    </form>
   );
 };
-
-export default ContactsForm;
+export default ContactForm;
