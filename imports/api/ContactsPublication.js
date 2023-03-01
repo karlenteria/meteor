7fa8ffb5c767392591ea4f684b870e777f4a85ea
Karl Enteria <karl.enteria@gmail.com>
2023-03-02 00:56:26 +0800

    added publication

---
+++ imports/api/ContactsPublication.js
@@ -0,0 +1,5 @@
+import { Meteor } from "meteor/meteor";
+import { ContactsCollection } from "./ContactsCollection";
+Meteor.publish("allContacts", function publishAllContacts() {
+  return ContactsCollection.find();
+});
--- server/main.js
+++ server/main.js
@@ -1,5 +1,5 @@
 import { Meteor } from "meteor/meteor";
 import "../imports/api/ContactsCollection";
 import "../imports/api/ContactsMethods";
-
+import "../imports/api/ContactsPublication";
 Meteor.startup(async () => {});
