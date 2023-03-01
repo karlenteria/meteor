9dcd5beafad19dc35e93f050ccc0d02c616be33f
Karl Enteria <karl.enteria@gmail.com>
2023-03-02 03:34:01 +0800

    added julianpostcss

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from "meteor/meteor";
 import "../imports/api/ContactsCollection";
-import "../imports/api/ContactsMethods";
+import "../imports/api/WalletsCollection";
+import "../imports/api/TransactionsCollection";
 import "../imports/api/ContactsPublication";
+import "../imports/api/ContactsMethods";
+
 Meteor.startup(async () => {});
